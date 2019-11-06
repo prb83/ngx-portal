1c7a636a93f60419f9fc27b670232560e96b6d1c
Patrick Brix <pab@censhare.com>
2019-11-06 11:49:41 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,20 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http'
-import { NgModule } from '@angular/core'
-import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material'
-import { BrowserModule } from '@angular/platform-browser'
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { ServiceWorkerModule } from '@angular/service-worker'
-import { StoreModule } from '@ngrx/store'
-import { StoreDevtoolsModule } from '@ngrx/store-devtools'
-import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io'
-import {
-  ConfigService,
-  NgxAuthInterceptorService,
-  NgxCoreModule,
-} from 'projects/ngx-core/src/public_api'
-import { AppMaterialModule } from 'projects/ngx-portal-app/src/app/app-material/app-material.module'
-import { NgxDashboardModule } from 'projects/ngx-portal-app/src/app/dashboard/dashboard.module'
-import { PagesModule } from 'projects/ngx-portal-web/src/app/pages/pages.module'
-import {
-  AppHeaderModule,
-  AppNavModule,
-  AppSidebarModule,
-  HeaderModule,
-  NgxComponentsModule,
-} from '../../../ngx-components/src/public_api'
-import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
-import { reducers } from './store/ngx.reducers'
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { MAT_SNACK_BAR_DEFAULT_OPTIONS } from '@angular/material';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { ConfigService, NgxAuthInterceptorService, NgxCoreModule } from 'projects/ngx-core/src/public_api';
+import { AppMaterialModule } from 'projects/ngx-portal-app/src/app/app-material/app-material.module';
+import { NgxDashboardModule } from 'projects/ngx-portal-app/src/app/dashboard/dashboard.module';
+import { PagesModule } from 'projects/ngx-portal-web/src/app/pages/pages.module';
+import { AppHeaderModule, AppNavModule, AppSidebarModule, HeaderModule, NgxComponentsModule } from '../../../ngx-components/src/public_api';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { reducers } from './store/ngx.reducers';
 
 const config: SocketIoConfig = {
   url: window.location.origin,
@@ -36,7 +26,7 @@ const appInitializerFn = (appConfig: ConfigService) => {
     return appConfig.loadAppConfig()
   }
 }
-@NgModule({
+@ NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,

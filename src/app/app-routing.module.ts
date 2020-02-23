48cd3db2adce79029edfd97336c5591a31d7957a
prb83 <brix1983@o2online.de>
2020-02-23 21:30:18 +0100

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,17 @@ import { NgxAuthGuardService, NgxRole } from 'projects/ngx-core/src/public_api'
 const routes: Routes = [
   {
     path: 'web',
-    loadChildren:
-      () => import('./../../../ngx-portal-web/src/app/pages/pages.module').then(m => m.PagesModule),
+    loadChildren: () =>
+      import('./../../../ngx-portal-web/src/app/pages/pages.module').then(
+        m => m.PagesModule
+      ),
   },
   {
     path: 'app',
-    loadChildren:
-      () => import('./../../../ngx-portal-app/src/app/dashboard/dashboard.module').then(m => m.NgxDashboardModule),
+    loadChildren: () =>
+      import(
+        './../../../ngx-portal-app/src/app/dashboard/dashboard.module'
+      ).then(m => m.NgxDashboardModule),
     canActivate: [NgxAuthGuardService],
     data: {
       expectedRole: NgxRole.Admin,
@@ -19,8 +23,10 @@ const routes: Routes = [
   },
   {
     path: 'app/dashboard',
-    loadChildren:
-      () => import('./../../../ngx-portal-app/src/app/dashboard/dashboard.module').then(m => m.NgxDashboardModule),
+    loadChildren: () =>
+      import(
+        './../../../ngx-portal-app/src/app/dashboard/dashboard.module'
+      ).then(m => m.NgxDashboardModule),
     canActivate: [NgxAuthGuardService],
     data: {
       expectedRole: NgxRole.Admin,
@@ -28,13 +34,17 @@ const routes: Routes = [
   },
   {
     path: 'app/account',
-    loadChildren:
-      () => import('./../../../ngx-portal-app/src/app/login/login.module').then(m => m.AppLoginModule),
+    loadChildren: () =>
+      import('./../../../ngx-portal-app/src/app/login/login.module').then(
+        m => m.AppLoginModule
+      ),
   },
   {
     path: 'app/todo',
-    loadChildren:
-      () => import('./../../../ngx-portal-app/src/app/todo/todo.module').then(m => m.NgxTodoModule),
+    loadChildren: () =>
+      import('./../../../ngx-portal-app/src/app/todo/todo.module').then(
+        m => m.NgxTodoModule
+      ),
     canActivate: [NgxAuthGuardService],
     data: {
       expectedRole: NgxRole.Admin,
@@ -42,8 +52,10 @@ const routes: Routes = [
   },
   {
     path: 'app/cms',
-    loadChildren:
-      () => import('./../../../ngx-portal-app/src/app/cms/cms.module').then(m => m.NgxCmsModule),
+    loadChildren: () =>
+      import('./../../../ngx-portal-app/src/app/cms/cms.module').then(
+        m => m.NgxCmsModule
+      ),
     canActivate: [NgxAuthGuardService],
     data: {
       expectedRole: NgxRole.Admin,
@@ -51,8 +63,10 @@ const routes: Routes = [
   },
   {
     path: 'app/media',
-    loadChildren:
-      () => import('./../../../ngx-portal-app/src/app/media/media.module').then(m => m.MediaModule),
+    loadChildren: () =>
+      import('./../../../ngx-portal-app/src/app/media/media.module').then(
+        m => m.MediaModule
+      ),
     canActivate: [NgxAuthGuardService],
     data: {
       expectedRole: NgxRole.Admin,
@@ -60,8 +74,10 @@ const routes: Routes = [
   },
   {
     path: 'app/weather',
-    loadChildren:
-      () => import('./../../../ngx-portal-app/src/app/weather/weather.module').then(m => m.NgxWeatherModule),
+    loadChildren: () =>
+      import('./../../../ngx-portal-app/src/app/weather/weather.module').then(
+        m => m.NgxWeatherModule
+      ),
     canActivate: [NgxAuthGuardService],
     data: {
       expectedRole: NgxRole.Admin,
@@ -69,8 +85,10 @@ const routes: Routes = [
   },
   {
     path: 'app/learn',
-    loadChildren:
-      () => import('./../../../ngx-portal-app/src/app/learn/learn.module').then(m => m.NgxLearnModule),
+    loadChildren: () =>
+      import('./../../../ngx-portal-app/src/app/learn/learn.module').then(
+        m => m.NgxLearnModule
+      ),
     canActivate: [NgxAuthGuardService],
     data: {
       expectedRole: NgxRole.Admin,
